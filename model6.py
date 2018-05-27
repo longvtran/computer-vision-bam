7afd71d9010131ff585c1e8e81bff7baa098a873
Long <long.tran92@gmail.com>
2018-05-26 21:29:53 -0700

    remove dense layer

--- model6.py
+++ model6.py
@@ -50,8 +50,8 @@ def ConvNet(num_classes_media=7, num_classes_emotion=4, training=False):
     x_media = tf.keras.layers.Flatten()(x_media)
     x_media = tf.keras.layers.Dense(units=128, 
                                     kernel_regularizer=tf.keras.regularizers.l2(1e-3),
-                                    activation='relu')(x_media)
-    x_media = tf.keras.layers.Dropout(rate=0.9)(x_media)
+                                    activation=tf.nn.relu)(x_media)
+#    x_media = tf.keras.layers.Dropout(rate=0.8)(x_media)
     output_media = tf.keras.layers.Dense(num_classes_media,
                                    kernel_initializer=initializer,
                                    activation='softmax',
@@ -70,10 +70,10 @@ def ConvNet(num_classes_media=7, num_classes_emotion=4, training=False):
                                  kernel_regularizer=tf.keras.regularizers.l2(1e-3))(x_emotion)
     x_emotion = tf.keras.layers.MaxPooling2D(2, 2)(x_emotion)
     x_emotion = tf.keras.layers.Flatten()(x_emotion)
-    x_emotion = tf.keras.layers.Dense(units=128, 
-                                      kernel_regularizer=tf.keras.regularizers.l2(1e-3),
-                                      activation='relu')(x_emotion)
-    x_emotion = tf.keras.layers.Dropout(rate=0.9)(x_emotion)
+#    x_emotion = tf.keras.layers.Dense(units=128, 
+#                                      kernel_regularizer=tf.keras.regularizers.l2(1e-3),
+#                                      activation='relu')(x_emotion)
+#    x_emotion = tf.keras.layers.Dropout(rate=0.8)(x_emotion)
     output_emotion = tf.keras.layers.Dense(num_classes_emotion,
                                      kernel_initializer=initializer,
                                      activation='softmax',
