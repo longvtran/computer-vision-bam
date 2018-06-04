3248a5556e812dcdf037a59d1e7aac1c3696acfb
Long <long.tran92@gmail.com>
2018-06-04 01:52:40 -0700

    increase dropout rate

--- model_vgg19.py
+++ model_vgg19.py
@@ -52,7 +52,7 @@ def VGG19(num_classes_media=7, num_classes_emotion=4, training=False):
     x_media = tf.keras.layers.Dense(units=1024, 
                                     kernel_regularizer=tf.keras.regularizers.l2(1e-3),
                                     activation=tf.nn.relu)(x_media)
-    x_media = tf.keras.layers.Dropout(rate=0.8)(x_media)
+    x_media = tf.keras.layers.Dropout(rate=0.85)(x_media)
     output_media = tf.keras.layers.Dense(num_classes_media,
                                    kernel_initializer=initializer,
                                    activation='softmax',
@@ -69,7 +69,7 @@ def VGG19(num_classes_media=7, num_classes_emotion=4, training=False):
     x_emotion = tf.keras.layers.Dense(units=1024, 
                                       kernel_regularizer=tf.keras.regularizers.l2(1e-3),
                                       activation=tf.nn.relu)(x_emotion)
-    x_emotion = tf.keras.layers.Dropout(rate=0.8)(x_emotion)
+    x_emotion = tf.keras.layers.Dropout(rate=0.75)(x_emotion)
     output_emotion = tf.keras.layers.Dense(num_classes_emotion,
                                      kernel_initializer=initializer,
                                      activation='softmax',
